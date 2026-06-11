c46a8d5c07d01dbcb2d79471bb85d71e83ce1c2c
Lemon <lewmonade04@gmail.com>
2026-06-11 22:56:15 +0700

    It Wỏk
    đã chạy đc 2 trường hợp:
     - 2 room id khác nhau ko bị dính tin nhắn
     - 1 room bật nhiều tab sẽ hiện tin nhắn mới nhắn (chưa kéo đc lịch sử tin nhắn)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,9 +3,11 @@ import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
 import { ChatService } from './services/chat.service';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
     ChatService
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,42 @@
-import { Routes } from '@angular/router';
+import {Router, Routes} from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+
+@Component({
+  standalone: true,
+  template: `<div style="text-align: center; margin-top: 100px; font-family: sans-serif;">
+               <h3>🔄 Đang khởi tạo phòng chat từ Docker Database...</h3>
+             </div>`,
+  imports: []
+})
+
+export class RouteInitializerComponent implements OnInit{
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit() {
+    // Gọi API của Spring Boot để tạo phòng mới tinh, lấy ID động từ DB dưới Docker
+    const defaultRoomPayload = { name: "Phòng Hệ Thống", members: [] };
+    this.http.post<any>('http://localhost:8080/api/chat/room', defaultRoomPayload)
+      .subscribe({
+        next: (roomFromDb) => {
+          console.log('--- DOCKER DB: Khởi tạo thành công phòng chat ID:', roomFromDb.id);
+          // Điều hướng trình duyệt sang URL động với ID vừa sinh ra từ DB
+          this.router.navigate(['/room', roomFromDb.id]);
+        },
+        error: (err) => {
+          console.error('Lỗi kết nối Backend Spring Boot:', err);
+        }
+      });
+  }
+}
+
 
 export const routes: Routes = [
-  // Cấu hình route động nhận tham số :id ở đây
+  // Vào trang chủ thì chạy Component thông minh để lấy ID động từ Docker
+  { path: '', component: RouteInitializerComponent },
+
+  // Route động bóc tách ID phòng chat
   { path: 'room/:id', component: ChatComponent },
 ];
